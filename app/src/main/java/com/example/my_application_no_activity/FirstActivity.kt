a1995b00a3549f2b82d5c99106c6821f0d75bd85
jiangrui <jiangrui@xinye.com>
2025-11-27 21:57:45 +0800

    activity

---
+++ app/src/main/java/com/example/my_application_no_activity/FirstActivity.kt
@@ -0,0 +1,59 @@
+package com.example.my_application_no_activity
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.first_layout)
+        val btn1: Button = findViewById(R.id.btn1);
+        btn1.setOnClickListener {
+            Toast.makeText(this, "Button_1 clicked", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return super.onOptionsItemSelected(item)
+        when (item.itemId) {
+            R.id.add_item -> Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show()
+        }
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
