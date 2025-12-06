42d04c6b844031e9284bd279b76fda67dd5c5165
16611 <1661126798@qq.com>
2025-12-07 01:50:50 +0800

    ListView的点击事件
     FruitAdapter 的 item 布局中包含了可获得焦点的控件（如 Button、CheckBox 等），这些控件默认会拦截父容器的点击事件。通过设置这些控件的 focusable 和 clickable 属性为 false 可以解决问题

--- app/src/main/java/com/example/my_application_no_activity/MainActivity3.kt
+++ app/src/main/java/com/example/my_application_no_activity/MainActivity3.kt
@@ -1,22 +1,42 @@
 package com.example.my_application_no_activity
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity3 : AppCompatActivity() {
-    private val data = listOf("Apple", "Banana", "Orange", "Watermelon",
-        "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango",
-        "Apple", "Banana", "Orange", "Watermelon", "Pear", "Grape",
-        "Pineapple", "Strawberry", "Cherry", "Mango");
+    private val data = listOf(
+        "Apple",
+        "Banana",
+        "Orange",
+        "Watermelon",
+        "Pear",
+        "Grape",
+        "Pineapple",
+        "Strawberry",
+        "Cherry",
+        "Mango",
+        "Apple",
+        "Banana",
+        "Orange",
+        "Watermelon",
+        "Pear",
+        "Grape",
+        "Pineapple",
+        "Strawberry",
+        "Cherry",
+        "Mango"
+    );
     private val fruitList = ArrayList<Fruit>();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_main3);
 
         val listView = findViewById<android.widget.ListView>(R.id.listView);
@@ -26,9 +46,17 @@ class MainActivity3 : AppCompatActivity() {
 //        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
 //        listView.adapter = adapter;
 
-        initFruits();
+        initFruits(); // 初始化水果数据
+//        item 布局中包含了可获得焦点的控件（如 Button、CheckBox 等），这些控件默认会拦截父容器的点击事件。
+//        通过设置这些控件的 focusable 和 clickable 属性为 false 可以解决问题
         val adapter = FruitAdapter(this, R.layout.fruit_item, fruitList);
         listView.adapter = adapter;
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val fruit = fruitList[position];
+            Log.d("--listItemClick--", "position: $position");
+            Toast.makeText(this, "You clicked ${fruit.name}", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun initFruits() {
