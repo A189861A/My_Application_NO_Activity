a2036d52a3d07c43c9ed8fdaf66378eae6fc07ef
jiangrui <jiangrui@xinye.com>
2026-02-28 17:57:12 +0800

    协程取消

--- app2/src/main/java/com/example/app2/MainActivity7.kt
+++ app2/src/main/java/com/example/app2/MainActivity7.kt
@@ -24,7 +24,7 @@ class MainActivity7 : AppCompatActivity() {
         * 协程作用域会自动跟随 Activity/Fragment 的生命周期，当 Activity/Fragment 销毁时，协程也会自动取消。
         * launch 是「非阻塞式」启动协程
         * */
-        lifecycleScope.launch {
+        val job = lifecycleScope.launch {
             // 协程默认运行在主线程（Dispatchers.Main）
             println("协程初始线程：${Thread.currentThread().name}")
 
@@ -34,7 +34,13 @@ class MainActivity7 : AppCompatActivity() {
             * 1.阻塞当前协程：会挂起当前协程直到代码块执行完成
             * 2.切换线程：可以在不同的线程调度器之间切换
             * 3.返回结果：代码块执行结束后，会返回代码块的返回值
+
+            调度器	                作用	                    适用场景
+            Dispatchers.Main	    主线程（Android）	        更新 UI、轻量计算
+            Dispatchers.IO	        IO 线程池（处理阻塞操作）	网络请求、文件读写、数据库操作
+            Dispatchers.Default	    默认线程池（CPU 密集型）	复杂计算、循环、数据处理
             * */
+
             val result = withContext(Dispatchers.IO) {
                 println("耗时操作线程：${Thread.currentThread().name}")
                 delay(2000) // 模拟网络请求/文件读取
@@ -50,7 +56,9 @@ class MainActivity7 : AppCompatActivity() {
             println("更新 UI 线程：${Thread.currentThread().name}" + "--data: $data")
         }
 
-
+        // 手动取消协程（比如点击按钮/页面销毁）
+        // job.cancel()
+        // job.join() // 等待协程取消完成（可选）
 
         // 协程是非阻塞的，这里会立即执行
         println("onCreate 执行完毕")
