076c14feb370a6f1ce3a274d4d086d25145f8aa7
jiangrui <jiangrui@xinye.com>
2026-02-28 17:47:10 +0800

    挂起函数（suspend）

--- app2/src/main/java/com/example/app2/MainActivity7.kt
+++ app2/src/main/java/com/example/app2/MainActivity7.kt
@@ -29,6 +29,12 @@ class MainActivity7 : AppCompatActivity() {
             println("协程初始线程：${Thread.currentThread().name}")
 
             // 2. 切换到 IO 线程执行耗时操作（网络/文件读写）,执行完自动切回原线程（主线程）
+            /*
+            * withContext 是 Kotlin 协程提供的一个挂起函数，用于切换协程的执行上下文（dispatcher）
+            * 1.阻塞当前协程：会挂起当前协程直到代码块执行完成
+            * 2.切换线程：可以在不同的线程调度器之间切换
+            * 3.返回结果：代码块执行结束后，会返回代码块的返回值
+            * */
             val result = withContext(Dispatchers.IO) {
                 println("耗时操作线程：${Thread.currentThread().name}")
                 delay(2000) // 模拟网络请求/文件读取
@@ -36,11 +42,30 @@ class MainActivity7 : AppCompatActivity() {
             }
 
             // 3. 自动切回主线程，更新 UI
-            println("更新 UI 线程：${Thread.currentThread().name}")
+            println("更新 UI 线程：${Thread.currentThread().name}" + "--result: $result")
             // textView.text = result
+
+            val data = fetchData() // 同步写法，异步执行
+            // 更新 UI
+            println("更新 UI 线程：${Thread.currentThread().name}" + "--data: $data")
         }
+
+
 
         // 协程是非阻塞的，这里会立即执行
         println("onCreate 执行完毕")
+    }
+
+    // 定义挂起函数（模拟网络请求）
+    /*
+    *
+    * 1. 挂起函数：只能在协程中调用，不能在普通函数中调用,实现 “可暂停的耗时逻辑”
+    * */
+    suspend fun fetchData(): String {
+        // 必须在协程中调用，否则编译报错
+        withContext(Dispatchers.IO) {
+            delay(1500)
+        }
+        return "从服务器获取的数据"
     }
 }
