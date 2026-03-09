77d23b413e741b97410539ac74849f25e1cd6bed
jiangrui <jiangrui@xinye.com>
2026-03-09 16:09:36 +0800

    向ViewModel传递参数

--- app3/src/main/java/com/example/app3/MainActivity4.kt
+++ app3/src/main/java/com/example/app3/MainActivity4.kt
@@ -1,5 +1,7 @@
 package com.example.app3
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
@@ -9,21 +11,49 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.app3.databinding.ActivityMain4Binding
 import com.example.app3.model.Main4ViewModel
+import com.example.app3.model.Main4ViewModelFactory
 
 class MainActivity4 : AppCompatActivity() {
     // 延迟初始化
     lateinit var viewModel: Main4ViewModel
     lateinit var binding: ActivityMain4Binding
+    /*
+     * # SharedPreferences:
+     * - 轻量级的存储类，主要用于保存应用程序的配置信息
+     * - 用于存储和检索简单的键值对数据。
+     * 使用 SharedPreferences 主要分为三步：
+           1. 获取对象：通过 Context 获取实例。
+           2. 获取编辑器：获取 Editor 对象用于写入数据。
+           3. 提交/应用：保存数据
+    * */
+    lateinit var sp: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /*
+        * getPreferences:获取 SharedPreferences 实例的一种便捷方式。
+        *  - 简单来说，它是 getSharedPreferences() 的“简化版”。
+        *  - 它会自动使用 当前 Activity 的类名 作为文件名（例如 MainActivity.xml）。
+        *  - 它创建的文件仅属于当前 Activity，通常不建议在其他地方访问它。
+        * */
+        sp = getPreferences(Context.MODE_PRIVATE)
+        val countReserved = sp.getInt("count_reserved", 0)
+
         /*
         * # ViewModelProvider: 它说ViewModel 的"供应商"或"管家"。
         *   - 创建、存储并提供 ViewModel 实例，并确保这些实例在配置更改（如屏幕旋转）时不会丢失，
         *       从而避免数据重复加载或状态丢失。
         *   - 确保获取到的 ViewModel 实例与特定的作用域（通常是 Activity 或 Fragment）绑定。
         * */
-        viewModel = ViewModelProvider(this).get(Main4ViewModel::class.java)
+//        viewModel = ViewModelProvider(this).get(Main4ViewModel::class.java)
+
+        /*
+        * ViewModelProvider：仓库管理员。
+        * owner：仓库的房间号（决定了 ViewModel 存在哪里）。
+        * factory：制造图纸（决定了 ViewModel 怎么造出来）。
+        * */
+        viewModel = ViewModelProvider(this,Main4ViewModelFactory(countReserved))
+            .get(Main4ViewModel::class.java)
         /*
         * #  Android ViewBinding（视图绑定）
         * - ActivityMain4Binding 是一个自动生成的类，它包含了对布局文件中所有视图的引用。
@@ -37,13 +67,16 @@ class MainActivity4 : AppCompatActivity() {
 
         binding.plusOneBtn.setOnClickListener {
             Log.d("--onClick--", "onClick:")
-            viewModel.count++
+            viewModel.counter++
             refreshCounter();
         }
-
+        binding.clearBtn.setOnClickListener {
+            viewModel.counter = 0
+            refreshCounter()
+        }
     }
 
     private fun refreshCounter() {
-        binding.infoText.text = viewModel.count.toString()
+        binding.infoText.text = viewModel.counter.toString()
     }
 }
--- app3/src/main/java/com/example/app3/model/Main4ViewModel.kt
+++ app3/src/main/java/com/example/app3/model/Main4ViewModel.kt
@@ -2,6 +2,9 @@ package com.example.app3.model
 
 import androidx.lifecycle.ViewModel
 
-class Main4ViewModel: ViewModel() {
-    var count = 0;
+/*
+* countReserved参数，用于记录之前保存的计数值，并在初始化的时候赋值给counter变量。
+* */
+class Main4ViewModel(conuntReserved: Int) : ViewModel() {
+    var counter = conuntReserved
 }
