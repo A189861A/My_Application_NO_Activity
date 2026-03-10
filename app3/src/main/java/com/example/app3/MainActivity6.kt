7a66fa80606956df337ade0c5e55f1e22a03053c
jiangrui <jiangrui@xinye.com>
2026-03-10 20:02:39 +0800

    Handler&Runnable

--- app3/src/main/java/com/example/app3/MainActivity6.kt
+++ app3/src/main/java/com/example/app3/MainActivity6.kt
@@ -45,6 +45,20 @@ class MainActivity6 : AppCompatActivity() {
             // 模拟用户ID变化（比如点击按钮切换）
             viewModel.getUserBtn("1001") // 触发加载张三的信息
             // 延迟后切换ID
+            /*
+            * Handler：主要用于线程间通信（尤其是子线程与 UI 线程的交互），
+            *   本质是通过 “消息队列（MessageQueue）+ 循环器（Looper）” 实现任务的异步调度
+            * Handler 构造函数：
+            *   - 传入 Looper 后，Handler 就知道应该把消息发送到哪个线程的消息队列
+            *   - postDelayed方法：延迟指定时间后 执行指定的任务.
+            *   - post方法：立即执行任务.
+            *   - sendEmptyMessageDelayed：延迟发送空消息.
+            *   - removeCallbacks：移除未执行的任务.
+            * Looper：
+            *   - 消息循环器，用于管理消息队列和处理消息
+            * Looper.getMainLooper()：
+            *   - 获取主线程（UI 线程）的 Looper 对象.
+            * */
             Handler(Looper.getMainLooper()).postDelayed({
                 viewModel.getUserBtn("1002") // 触发加载李四的信息
             }, 1000)
@@ -52,3 +66,50 @@ class MainActivity6 : AppCompatActivity() {
     }
 
 }
+
+
+/*
+* Runnable 核心定义：
+*   - 函数式接口（仅含一个抽象方法run）
+*   - 是 Android 中处理异步任务、线程切换的核心接口.
+*   - 本身不启动线程，需配合 Thread、Handler 等使用，常用于 UI 线程与子线程的交互.
+
+    val handler = Handler(Looper.getMainLooper())
+    val runnable = object : Runnable {
+        override fun run() {
+            // 切换到下一张图片
+            viewPager.currentItem += 1
+            handler.postDelayed(this, 3000) // 每 3 秒切换一次
+        }
+    }
+    // Android 规定只能在 UI 线程更新控件，
+    // 子线程需通过 Handler + Runnable 切换到 UI 线程更新 UI.
+    handler.postDelayed(runnable, 3000)
+
+*
+* */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app3/src/main/java/com/example/app3/model/Main4ViewModelFactory.kt
+++ app3/src/main/java/com/example/app3/model/Main4ViewModelFactory.kt
@@ -9,7 +9,11 @@ import androidx.lifecycle.ViewModelProvider
 *   - 接口只有一个方法：create
 * */
 class Main4ViewModelFactory(private val countReserved: Int) : ViewModelProvider.Factory {
+    // @Suppress("UNCHECKED_CAST") 是 Kotlin 中的一个注解，告诉编译器忽略"不安全类型转换"警告 。
+    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return Main4ViewModel(countReserved) as T
+        // 类型转换为请求的ViewModel类型
+        // as T： 这种强制转换在 编译时无法检查安全性 ，所以会产生 "unchecked cast" 警告
+        return Main4ViewModel(countReserved) as T 
     }
 }
