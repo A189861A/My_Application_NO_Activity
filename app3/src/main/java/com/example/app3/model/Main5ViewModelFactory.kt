7c67d6da74cdbf5691a42a9cb4664b4941e74a32
jiangrui <jiangrui@xinye.com>
2026-06-24 14:03:31 +0800

    Jetpack Compose 组件

--- app3/src/main/java/com/example/app3/model/Main5ViewModelFactory.kt
+++ app3/src/main/java/com/example/app3/model/Main5ViewModelFactory.kt
@@ -4,14 +4,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 /*
-* ViewModelProvider.Factory：
+* ViewModelProvider.Factory：Android 中的一个接口，这里表示 Main5ViewModelFactory 实现了该接口
 *   - 创建ViewModel实例的工厂
 *   - 接口只有一个方法：create
 * */
 class Main5ViewModelFactory(private val countReserved: Int) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         /*
-        *
         * 检查请求的ViewModel类是否是MainViewModel的子类
         * */
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
--- sunnyweather/src/main/java/com/example/sunnyweather/model/WeatherResponse.kt
+++ sunnyweather/src/main/java/com/example/sunnyweather/model/WeatherResponse.kt
@@ -4,10 +4,25 @@ package com.example.sunnyweather.model
 /*
 * data class ：是 Kotlin 中专门用于存储数据的类，
 *   它会自动为你生成一系列实用的方法，让你能够更简洁、更高效地处理数据。
-* 自动生成：equals()、hashCode()、toString()、copy() 等方法。
+*   自动生成：equals()、hashCode()、toString()、copy(),解构赋值 等方法。
+*   -- 主构造函数必须至少有一个参数
+*   -- 仅主构造参数参与自动生成方法，类内额外属性不参与
+*
+*   data class User(
+        val id: Int,
+        val name: String,
+        var age: Int
+    )
+    # 结构赋值：
+        val u = User(1,"张三",20)
+        val (uid, uname, uage) = u
+    # 仅主构造参数参与自动生成方法，类内额外属性不参与
+    data class A(val a:Int){
+        var b:Int = 0 // b 不参与 equals/toString
+    }
 * */
 data class WeatherResponse(
-    val result: Result
+    val result: Result // 主构造函数
 )
 
 data class Result(
